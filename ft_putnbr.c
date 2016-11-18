3500a3f8e6e5098cfe11b44fe612065642973a11
Thomas GIRAUD <thgiraud@e1r12p21.42.fr>
2016-11-18 14:21:28 +0100

    MODIFlastCHECK

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: thgiraud <thgiraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/05 17:50:38 by thgiraud          #+#    #+#             */
-/*   Updated: 2016/11/11 19:59:31 by thgiraud         ###   ########.fr       */
+/*   Updated: 2016/11/18 13:42:22 by thgiraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@ void	ft_putnbr(int n)
 {
 	if (n == -2147483648)
 		ft_putstr("-2147483648");
-	if (n < 0)
-		n = -n;
-	else if (n < 10)
-		ft_putchar(n + 48);
 	else
 	{
-		ft_putnbr(n / 10);
-		ft_putnbr(n % 10);
+		if (n < 0)
+		{
+			ft_putchar('-');
+			n *= -1;
+		}
+		if (n >= 10)
+			ft_putnbr(n / 10);
+		ft_putchar((n % 10) + '0');
 	}
 }
--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: thgiraud <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 14:37:03 by thgiraud          #+#    #+#             */
-/*   Updated: 2016/11/12 16:26:34 by thgiraud         ###   ########.fr       */
+/*   Updated: 2016/11/18 13:50:41 by thgiraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,16 @@
 
 char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
-	size_t	i;
-	size_t	j;
+	size_t	s1_i;
+	size_t	s2_i;
 
-	i = 0;
-	j = 0;
-	while (s1[i])
-		i++;
-	while (j < n && s2[j])
+	s1_i = ft_strlen(s1);
+	s2_i = -1;
+	while (s2[++s2_i] && s2_i < n)
 	{
-		s1[i] = s2[j];
-		j++;
-		i++;
+		s1[s1_i] = s2[s2_i];
+		s1_i++;
 	}
-	if (j == ft_strlen(s2))
-		s1[i] = '\0';
+	s1[s1_i] = '\0';
 	return (s1);
 }
