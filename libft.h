b6ac2479508d95f149d57f3f92dceceacb0c8dcb
Thomas GIRAUD <thgiraud@e2r5p13.42.fr>
2016-11-26 15:24:13 +0100

    .h+NDUP

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: thgiraud <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 14:35:37 by thgiraud          #+#    #+#             */
-/*   Updated: 2016/11/24 17:48:42 by thgiraud         ###   ########.fr       */
+/*   Updated: 2016/11/26 15:23:22 by thgiraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,6 +84,7 @@ void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+char				*ft_strndup(const char *s1, size_t n);
 
 #endif
 
